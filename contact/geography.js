a91674f70899e3666cc37856af590c5dacaa8e14
Gardner-Anonymous <154289835+uv1tgas@users.noreply.github.com>
2023-12-19 18:56:59 -0800

    c

--- contact/geography.js
+++ contact/geography.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/reviews/',
-    bare: '/outerspace/',
+    prefix: '/sw/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/contact/algebra.js',
